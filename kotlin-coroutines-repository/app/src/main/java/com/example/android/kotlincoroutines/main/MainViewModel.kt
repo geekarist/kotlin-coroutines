a456651b7b4d8e95a1f461d449129f41fff9fe8d
Christobal <geekarist@users.noreply.github.com>
2019-04-19 07:50:16 +0200

    Complete step 7

--- kotlin-coroutines-repository/app/src/main/java/com/example/android/kotlincoroutines/main/MainViewModel.kt
+++ kotlin-coroutines-repository/app/src/main/java/com/example/android/kotlincoroutines/main/MainViewModel.kt
@@ -19,10 +19,9 @@ package com.example.android.kotlincoroutines.main
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.example.android.kotlincoroutines.main.TitleRepository.RefreshState.Error
-import com.example.android.kotlincoroutines.main.TitleRepository.RefreshState.Loading
-import com.example.android.kotlincoroutines.main.TitleRepository.RefreshState.Success
+import androidx.lifecycle.viewModelScope
 import com.example.android.kotlincoroutines.util.singleArgViewModelFactory
+import kotlinx.coroutines.launch
 
 /**
  * MainViewModel designed to store and manage UI-related data in a lifecycle conscious way. This
@@ -100,18 +99,14 @@ class MainViewModel(private val repository: TitleRepository) : ViewModel() {
     /**
      * Refresh the title, showing a loading spinner while it refreshes and errors via snackbar.
      */
-    // TODO: Change this implementation to use coroutines
-    fun refreshTitle() {
-        // pass a state listener as a lambda to refreshTitle
-        repository.refreshTitle { state ->
-            when (state) {
-                is Loading -> _spinner.postValue(true)
-                is Success -> _spinner.postValue(false)
-                is Error -> {
-                    _spinner.postValue(false)
-                    _snackBar.postValue(state.error.message)
-                }
-            }
+    private fun refreshTitle() = viewModelScope.launch {
+        try {
+            _spinner.postValue(true)
+            repository.refreshTitle()
+            _spinner.postValue(false)
+        } catch (t: Throwable) {
+            _spinner.postValue(false)
+            _snackBar.postValue(t.message)
         }
     }
 
